35cb1ab1709b0e2c564a93053ff541d84ee8e340
Yeison Moran <moranyeison522@gmail.com>
2026-04-08 23:28:59 -0500

    fix: reemplazar submódulo src/data por archivos normales

---
+++ src/data/productos.js
@@ -0,0 +1,12 @@
+//Base de datos simulada
+
+const productos = [
+    { id: 1, nombre: 'Torta de Chocolate', precio: 45000, disponible: true },
+    { id: 2, nombre: 'Cheesecake de Fresa', precio: 38000, disponible: true },
+    { id: 3, nombre: 'Cupcakes x6', precio: 22000, disponible: true },
+    { id: 4, nombre: 'Macarons x12', precio: 30000, disponible: false },
+    { id: 5, nombre: 'Torta Red Velvet', precio: 50000, disponible: true },
+
+];
+
+module.exports = productos;
